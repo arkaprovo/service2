753c9c539e6d691c19df8cdfea7e074abf9e1280
arkaprovob <arkaprovo.bhattacharjee@sath.com>
2017-11-13 00:01:46 +0530

    Initial commit

---
+++ src/main/kotlin/com/sath/devops/service2/Service2Application.kt
@@ -0,0 +1,11 @@
+package com.sath.devops.service2
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Service2Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Service2Application::class.java, *args)
+}
---
+++ src/main/kotlin/com/sath/devops/service2/service/Service2Service.kt
@@ -0,0 +1,12 @@
+package com.sath.devops.service2.service
+
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
+import org.springframework.stereotype.Service
+
+@Service
+class Service2Service {
+
+    val log: Log? = LogFactory.getLog(this.javaClass);
+
+}
